cf02f4b697b3514a92f155de18cf006c61d2619f
goose <dava170103@gmail.com>
2023-12-29 13:38:07 +0600

    1

--- src/tests/test_1.py
+++ src/tests/test_1.py
@@ -1,3 +1,10 @@
+import pytest
+from selenium.webdriver.chrome import webdriver
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver
+
 class test1:
     def test_1(self, set_up_browser):
         driver = set_up_browser
