e6f69a2f0ec328f746ca9f9a2a8ecf346d1025d8
goose <dava170103@gmail.com>
2024-01-18 20:32:44 +0600

    1

--- src/tests/test_1.py
+++ src/tests/test_1.py
@@ -1,16 +1,10 @@
-import pytest
-from selenium.webdriver.chrome import webdriver
-
-
-@pytest.fixture
-def set_up_browser(request):
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 class Test:
-    def test(self, set_up_browser):
-        driver = set_up_browser
-        driver.get("https://skillbox.ru")
-        assert 'Skillbox - образовательная платформа с онлайн-курсами' == driver.title
+    def test(self):
+        options = ChromeOptions()
+        driver = Chrome(options=options)
+        driver.get('https://skillbox.ru')
+        assert 'Skillbox – образовательная платформа с онлайн-курсами.' == driver.title
